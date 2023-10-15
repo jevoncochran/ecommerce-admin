0ef4a4724233a262823f2b6fcd636f5fec20966c
Jevon Cochran <jevon@securebags.com>
2023-10-15 12:42:09 -0500

    connect categories to seller

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,17 +1,21 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/category";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function POST(req: Request) {
   await mongooseConnect();
 
-  const body = await req.json();
-  const { name, parentCategory, properties } = body;
-
   try {
+    const session = await getServerSession(authOptions);
+    const body = await req.json();
+    const { name, parentCategory, properties } = body;
+
     const category = await Category.create({
       name,
       parentCategory,
       properties,
+      seller: session?.user?._id,
     });
     return new Response(JSON.stringify(category), { status: 201 });
   } catch (error) {
@@ -23,20 +27,37 @@ export async function GET() {
   await mongooseConnect();
 
   try {
-    const categories = await Category.find({}).populate("parentCategory");
+    const session = await getServerSession(authOptions);
+
+    const categories = await Category.find({
+      seller: session?.user?._id,
+    }).populate("parentCategory");
     return new Response(JSON.stringify(categories), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify(error), { status: 500 });
   }
 }
 
+// TODO: Wondering if this should be in a separate route
+// Suggested route: /api/categories/[id]/route.ts
 export async function PUT(req: Request) {
   await mongooseConnect();
 
-  const body = await req.json();
-  const { _id, name, parentCategory, properties } = body;
-
   try {
+    const session = await getServerSession(authOptions);
+
+    const body = await req.json();
+    const { _id, name, parentCategory, properties } = body;
+
+    const categoryById = await Category.findById(_id);
+
+    if (categoryById.seller?.toString() !== session?.user?._id) {
+      return new Response(
+        JSON.stringify({ errorMessage: "Unauthorized request" }),
+        { status: 401 }
+      );
+    }
+
     const category = await Category.updateOne(
       { _id },
       { name, parentCategory, properties }
--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,11 +1,17 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/order";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET(req: Request) {
   await mongooseConnect();
 
+  const session = await getServerSession(authOptions);
+
   try {
-    const orders = await Order.find({}).sort({ createdAt: -1 });
+    const orders = await Order.find({ seller: session?.user?._id }).sort({
+      createdAt: -1,
+    });
 
     return new Response(JSON.stringify(orders), { status: 200 });
   } catch (error) {
